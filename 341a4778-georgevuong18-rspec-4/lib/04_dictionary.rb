20a3fc196fc2eec5610b96890da76afd844838a9
George V <georgevuong18@gmail.com>
2017-05-19 12:07:39 -0700

    completed 04_dictionary.rb in rspec4

--- 341a4778-georgevuong18-rspec-4/lib/04_dictionary.rb
+++ 341a4778-georgevuong18-rspec-4/lib/04_dictionary.rb
@@ -1,3 +1,54 @@
 class Dictionary
-  # TODO: your code goes here!
+
+  def initialize
+    @d = {} # dictionary instance variable
+  end
+
+  def entries # getter method
+    @d
+  end
+
+  def add(animal)
+    @d[animal.keys.first] = animal.values.first if animal.class == Hash
+    @d[animal] = nil if animal.class == String
+  end
+
+  def keywords
+    @d.keys.sort
+  end
+
+  def include?(keyword)
+    @d.include?(keyword)
+  end
+
+  def find(str)
+    return {} if @d.empty?
+    #return {} unless @d.include?(str)
+    #return @d if @d.include?(str)
+    arr = []
+    @d.to_a.each do |sub_arr|
+      if sub_arr.first.include?(str)
+        arr << sub_arr
+      else
+        next
+      end
+    end
+
+    arr.to_h
+  end
+
+  def printable
+    str = ""
+    last_key = @d.keys.sort.last
+    @d = @d.sort_by { |key, _| key } # no longer a hash
+    p @d
+    @d.each do |key, val|
+      str << "[#{key}]" + " "
+      str << "\"#{val}\""
+      str << "\n" unless key == last_key
+    end
+
+    str
+  end
+
 end
