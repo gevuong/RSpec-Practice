7deda5564f66eaf33b27c4e0fb4a2d578a28b4cd
George V <georgevuong18@gmail.com>
2017-05-26 14:49:11 -0700

    passed specs in rspec4/05_my_hash_set_spec.rb

--- 341a4778-georgevuong18-rspec-4/lib/05_my_hash_set.rb
+++ 341a4778-georgevuong18-rspec-4/lib/05_my_hash_set.rb
@@ -28,28 +28,20 @@
 # Write a `set1#minus(set2)` method which returns a new set which includes
 # all the items of `set1` that aren't in `set2`.
 
+# NOTE to prove how well I understand OOP.
+# When new objects, set1 and set2, are created using MyHashSet.new, the initialized method is called, and the object variable, @store, which contains an empty hash, is stored in the newly created objects, set1 and set2.
+# Both objects, set1 and set2, now contain the object variable, @store.
+# @store object variable is only within the scope of the current object, set1. Now, in order to access the object variable, @store within object set1,
+
 class MyHashSet
+  attr_reader :store
+
   def initialize
     @store = {}
   end
 
-  def minus(set2)
-    set2
-  end
-
-  def intersect(set2)
-    # p @store
-    # p set2
-  end
-
-  def union(set2)
-    p @store.merge(set2)
-    #p set2.class
-    #set2.to_set
-  end
-
-  def to_a
-    @store.keys # returns an array of keys items
+  def insert(el)
+    @store[el] = true
   end
 
   def include?(el)
@@ -59,17 +51,39 @@ class MyHashSet
   def delete(el)
     if include?(el)
       @store.delete(el)
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
-  def insert(el)
-    @store[el] = true
+  def to_a
+    @store.keys # returns an array of keys items
   end
 
+  def union(set2) # return set containing elements in either set1 or set2, no repeated elements.
+    new_set = MyHashSet.new
+    # p self.class
+    # p self
+    set1 = self.store.keys.to_set
+    set3 = set1.merge(set2.store.keys.to_set)
+    set3.each { |el| new_set.store[el] = true }
+    p new_set
+  end
 
+  def intersect(set2) # return set containing elements that exist in both set1 and set2.
+    new_set = MyHashSet.new
+    set3 = self.store.select { |k, _| set2.store.key?(k) } .keys
+    set3.each { |el| new_set.store[el] = true }
+    new_set
+  end
+
+  def minus(set2) # return set containing elements only in first set.
+    new_set = MyHashSet.new
+    set3 = self.store.reject { |k, _| set2.store.key?(k) } .keys
+    set3.each { |el| new_set.store[el] = true }
+    new_set
+  end
 end
 
 # Bonus
